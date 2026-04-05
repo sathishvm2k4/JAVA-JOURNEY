01dd2388ff440c47b354d24e63090bb67189071b
Sathish V M <2k4satishvm@gmail.com>
2026-04-05 10:16:59 +0530

    Added Day 1 and Day 2 Java files

---
+++ Day_01_java/Hellojava.java
@@ -0,0 +1,8 @@
+package Day_01_java;
+
+public class Hellojava {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am starting my Java journey!");
+        System.out.println("Excited to learn and grow 🚀");
+    }
+}
---
+++ Day_02_java/JavaExecutionFlow.java
@@ -0,0 +1,13 @@
+package Day_02_java;
+
+public class JavaExecutionFlow {
+    public static void main(String[] args) {
+
+        System.out.println("Step 1: Write Java code (.java file)");
+        System.out.println("Step 2: Compile → bytecode (.class file)");
+        System.out.println("Step 3: Run using JVM");
+        System.out.println("Java is platform independent 🚀");
+
+    }
+
+}
