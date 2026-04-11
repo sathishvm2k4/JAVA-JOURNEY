a6840294a9c1f689309146380096f7a0d76d3e82
Sathish V M <2k4satishvm@gmail.com>
2026-04-11 20:33:49 +0530

    updated day 8

--- Day_08_java/LargestNumber.java
+++ Day_08_java/LargestNumber.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class LargestNumber {
        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the First number:");
         int a = sc.nextInt();
+         System.out.println("Enter the Second number:");
         int b = sc.nextInt();
+         System.out.println("Enter the Third number:");
         int c = sc.nextInt();
 
         if(a >= b && a >= c) {
-            System.out.println("Largest: " + a);
+            System.out.println("Largest number: " + a);
         } else if(b >= c) {
-            System.out.println("Largest: " + b);
+            System.out.println("Largest number: " + b);
         } else {
-            System.out.println("Largest: " + c);
+            System.out.println("Largest number: " + c);
         }
     }
 }
