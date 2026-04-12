d25cebece5f89452592680f48c168877cd811042
Sathish V M <2k4satishvm@gmail.com>
2026-04-12 22:01:21 +0530

    Day 09 add

---
+++ Day_09_java/Table.java
@@ -0,0 +1,16 @@
+package Day_09_java;
+
+import java.util.Scanner;
+
+public class Table {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num*i));
+        }
+    }
+    
+}
